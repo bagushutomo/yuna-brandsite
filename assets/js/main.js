43c714e5bfee430dac6491339946fb26b339bc21
Bagus Hutomo <bagus.hutomo@gmail.com>
2017-12-13 17:19:58 +0700

    saved without animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,13 @@
 	$('#js_nav_expand').click(function(){
 		$(this).toggleClass('open');
 		$('#js_nav').toggleClass('collapsed');
+		$('body').toggleClass('is_menu_open');
+	});
+
+	$('.menu_item_link').click(function(e) {
+		$('#js_nav_expand').toggleClass('open');
+		$('#js_nav').toggleClass('collapsed');
+		$('body').toggleClass('is_menu_open');
+		return true;
 	});
 })(jQuery);
